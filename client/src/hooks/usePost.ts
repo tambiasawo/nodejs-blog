af8bbbe624268cabc1c6368041a7bbe71f798eef
Tambi <tambi.asawo@verticalmotion.ca>
2025-07-01 10:35:28 -0700

    feat: add hooks

---
+++ client/src/hooks/usePost.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect, useCallback } from "react";
+const URL = "/v1";
+type Post = {
+  title: string;
+  body: string;
+  updatedAt: string;
+  _id: string;
+};
+const usePost = (id: string) => {
+  const [post, setPost] = useState<Post | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const getPost = useCallback(async (id: string) => {
+    if (!id) return;
+
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`${URL}/posts/${id}`, {
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error(
+          `Fetch failed: ${response.status} ${response.statusText}`
+        );
+      }
+      const data = await response.json();
+      setPost(data);
+    } catch (e) {
+      console.error(e);
+      setError(e instanceof Error ? e.message : "Sth undexpected went ");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPost(id);
+  }, [getPost, id]);
+
+  return { post, loading, error };
+};
+
+export default usePost;
---
+++ client/src/hooks/usePosts.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect, useCallback } from "react";
+const URL = "v1";
+export type Post = {
+  title: string;
+  body: string;
+  updatedAt: string;
+  _id: string;
+};
+const usePosts = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+  const [total, setTotal] = useState();
+
+  const getPosts = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`${URL}/posts`);
+      const posts = await response.json();
+      setPosts(posts.posts);
+      setTotal(posts.count);
+    } catch (e) {
+      console.error(e);
+      setError(e instanceof Error ? e.message : "Sth undexpected went ");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  return { posts, loading, error, refresh: getPosts };
+};
+
+export default usePosts;
