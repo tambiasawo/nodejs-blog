eab75f099c2d6248809d945a667bf601b6cd04fa
Tambi <tambi.asawo@verticalmotion.ca>
2025-07-01 18:35:18 -0700

    fix: cors error path

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,3 @@
-/* import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-
-import version1API from "../src/routes/versions/v1/api";
-const app = express();
-
-//middlewares
-app.use(cookieParser()); // to read saved cookies
-app.use(express.json()); // enable json body in fetch requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("../public")); // set the dir of our public folder
-app.use(cors({ origin: "http://localhost:5143/v1" })); //our frontend endpoint
-
-app.use("/v1", version1API);
-//app.use(express.static(path.resolve(__dirname, "../../client/dist")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
-});
-
-export default app;
- */
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
@@ -34,10 +12,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.options(
+/* app.options(
   "/v1/*",
   cors({ origin: "https://nodejs-blog-1-i9ga.onrender.com", credentials: true })
-);
+); */
 app.use(
   "/v1",
   cors({
