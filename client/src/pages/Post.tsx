4d861e1f9b53106e886405a0e4ca56015ff61e73
Tambi <tambi.asawo@verticalmotion.ca>
2025-07-02 18:14:19 -0700

    fix: null beheaior

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,6 +1,7 @@
 import usePost from "../hooks/usePost";
 import { useParams } from "react-router-dom";
 import "../post.css";
+import React from "react";
 
 const makeParagraphs = (text: string, wordsPerPara = 100) => {
   const words = text?.split(" ");
@@ -15,8 +16,12 @@ const Post = () => {
   const { id } = useParams();
 
   const { post } = usePost(id as string);
-  const paras = makeParagraphs(post?.body as string, 80);
+  const [paras, setParas] = React.useState<string[]>([]);
 
+  React.useEffect(() => {
+    if (post?.body) setParas(makeParagraphs(post.body as string, 80));
+  }, [post]);
+  
   return (
     <div>
       <h3>{post?.title}</h3>
