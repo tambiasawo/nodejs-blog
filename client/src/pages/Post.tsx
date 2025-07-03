ba1d86cd0dc192c72b485cee3e1a849214336668
Tambi <tambi.asawo@verticalmotion.ca>
2025-07-02 19:02:36 -0700

    cleanup

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -1,22 +1,21 @@
 import usePost from "../hooks/usePost";
 import { useParams } from "react-router-dom";
 import "../post.css";
-import React from "react";
 
-const makeParagraphs = (text: string, wordsPerPara = 100) => {
+/* const makeParagraphs = (text: string, wordsPerPara = 100) => {
   const words = text?.split(" ");
   const paras = [];
   for (let i = 0; i < words.length; i += wordsPerPara) {
     paras.push(words.slice(i, i + wordsPerPara).join(" "));
   }
   return paras;
-};
+}; */
 
 const Post = () => {
   const { id } = useParams();
 
   const { post } = usePost(id as string);
-  const [paras, setParas] = React.useState<string[]>([]);
+  //const [paras, setParas] = React.useState<string[]>([]);
 
   /*  React.useEffect(() => {
     if (post?.body) setParas(makeParagraphs(post.body as string, 80));
